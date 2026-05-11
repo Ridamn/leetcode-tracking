b25b46acacfe08a69f05789b1bd1aab7a30a0fc0
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:55:00 +0530

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.java
@@ -0,0 +1,16 @@
+// Last updated: 5/11/2026, 9:55:00 AM
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder res = new StringBuilder();
+        res.append("a");   
+        while(res.length()<k){
+            StringBuilder sb = new StringBuilder();
+            String temp = res.toString();
+            for(char j : temp.toCharArray()){
+                sb.append((char) (j+1));
+            }
+            res.append(sb);
+        }
+        return res.charAt(k-1);
+    }
+}
