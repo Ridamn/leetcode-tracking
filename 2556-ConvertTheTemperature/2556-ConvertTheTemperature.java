e84d3c77c4c7992f067f86b724aa1a464166616f
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:55:27 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 5/11/2026, 9:55:26 AM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[] { celsius + 273.15, celsius * 1.80 + 32.00 };
+    }
+}
