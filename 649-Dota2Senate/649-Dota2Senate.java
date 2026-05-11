ae0cc2e55a7ba31f588474184e516b7402ece7cf
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:57:24 +0530

    Create 649-Dota2Senate

---
+++ 649-Dota2Senate/649-Dota2Senate.java
@@ -0,0 +1,24 @@
+// Last updated: 5/11/2026, 9:57:23 AM
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> rad = new LinkedList<>(), dir = new LinkedList<>();
+        int n = senate.length();
+        for (int i = 0; i < n; i++){
+            if (senate.charAt(i) == 'R'){
+                rad.add(i);
+            } else {
+                dir.add(i);
+            }
+        }
+        while (!rad.isEmpty() && !dir.isEmpty()){
+            if (rad.peek() < dir.peek()){
+                rad.add(n++);
+            } else {
+                dir.add(n++);
+            }
+            rad.poll();
+            dir.poll();
+        }
+        return (rad.isEmpty()) ? ("Dire") : ("Radiant");
+    }
+}
