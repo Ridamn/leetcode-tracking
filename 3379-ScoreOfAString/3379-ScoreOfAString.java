f9c0566fae6a521553f7d13ffe311709e9b18adb
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:55:06 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,10 @@
+// Last updated: 5/11/2026, 9:55:05 AM
+class Solution {
+    public int scoreOfString(String s) {
+        int n=0;
+        for(int i=0; i< s.length()-1; i++){
+            n += Math.abs(s.charAt(i) - s.charAt(i+1));
+        }
+        return n;
+    }
+}
