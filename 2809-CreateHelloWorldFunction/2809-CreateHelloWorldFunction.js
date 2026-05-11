864b8550b07a93f148a02aa3124ce507ab3a10d3
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:55:22 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 5/11/2026, 9:55:21 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
