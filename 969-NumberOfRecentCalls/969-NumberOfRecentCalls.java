28d607f3df41de66567e7c42ab18000887181fd5
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:56:58 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,18 @@
+// Last updated: 5/11/2026, 9:56:57 AM
+import java.util.*;
+
+class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        q.add(t);
+        while (q.peek() < t - 3000) {
+            q.poll();
+        }
+        return q.size();
+    }
+}
