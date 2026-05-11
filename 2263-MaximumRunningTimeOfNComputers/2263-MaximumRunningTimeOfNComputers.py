60e595c6664920aacd34f206bcd3155e8746a9f8
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:55:48 +0530

    Create 2263-MaximumRunningTimeOfNComputers

---
+++ 2263-MaximumRunningTimeOfNComputers/2263-MaximumRunningTimeOfNComputers.py
@@ -0,0 +1,15 @@
+# Last updated: 5/11/2026, 9:55:47 AM
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        l, r, ans=min(batteries), sum(batteries)//n, 0
+        while l<=r:
+            mid=(l+r)>>1
+            reserve=0
+            for x in batteries: reserve+=min(x, mid)
+            if reserve>=mid*n:
+                ans=mid
+                l=mid+1
+            else:
+                r=mid-1
+        return ans
+        
