d06af1802ee296b4926328fb730d96b06a96d5e3
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-11 09:53:22 +0530

    Create 4175-SmallestAllOnesMultiple

---
+++ 4175-SmallestAllOnesMultiple/4175-SmallestAllOnesMultiple.java
@@ -0,0 +1,16 @@
+// Last updated: 5/11/2026, 9:53:21 AM
+class Solution {
+    public int minAllOneMultiple(int k) {
+
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+
+        int r = 0;
+
+        for (int i = 1; i <= k; i++) {
+            r = (r * 10 + 1) % k;
+            if (r == 0) return i;
+        }
+
+        return -1;
+    }
+}
