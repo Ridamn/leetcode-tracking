4cea9986cc1e3375c4f8b1058834b8ee98a1cc7a
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-05-19 08:42:08 +0530

    Create 1760-MinimumLimitofBallsinaBag

---
+++ 1760-MinimumLimitofBallsinaBag/version/java/1760-MinimumLimitofBallsinaBag_20260519_084206.java
@@ -0,0 +1,28 @@
+// Last updated: 5/19/2026, 8:42:06 AM
+1class Solution {
+2    public int minimumSize(int[] nums, int max) {
+3        int p1 = 1;
+4        int p2 = 0;
+5
+6        for(int i : nums) p2 = Math.max(p2, i);
+7
+8        while(p1 < p2){
+9            int mid = p1 + (p2-p1)/2;
+10
+11            if(check(nums, max, mid)) p2 = mid;
+12            else p1 = mid + 1;
+13        }
+14        return p1;
+15    }
+16    public boolean check(int[] nums, int max, int mid){
+17        int total = 0;
+18
+19        for(int i : nums){
+20            int c = (int) Math.ceil(i/(double)mid)-1;
+21            total += c;
+22
+23            if(total > max) return false;
+24        }
+25        return true;
+26    }
+27}
