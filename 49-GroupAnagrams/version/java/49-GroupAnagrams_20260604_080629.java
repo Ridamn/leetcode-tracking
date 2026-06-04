78f6212805aa4f91dafb02c5c644d93b4b4a26f5
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-06-04 08:06:30 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/version/java/49-GroupAnagrams_20260604_080629.java
@@ -0,0 +1,17 @@
+// Last updated: 6/4/2026, 8:06:29 AM
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> arr = new HashMap<>();
+4        
+5        for(String i : strs){
+6            char[] chars = i.toCharArray();
+7            Arrays.sort(chars);
+8            String sorted = new String(chars);
+9            if(!arr.containsKey(sorted)){
+10                arr.put(sorted, new ArrayList<>());
+11            }
+12            arr.get(sorted).add(i);
+13        }
+14        return new ArrayList<>(arr.values());
+15    }
+16}
