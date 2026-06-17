0984ce0aafb98e6579c14f1d64ffb82c9b6e1959
Ridam Saxena <164179123+Ridamn@users.noreply.github.com>
2026-06-17 08:43:35 +0530

    Create 2128-ReversePrefixOfWord

---
+++ 2128-ReversePrefixOfWord/2128-ReversePrefixOfWord.java
@@ -0,0 +1,20 @@
+// Last updated: 6/17/2026, 8:43:34 AM
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int idx = word.indexOf(ch);
+
+        if (idx == -1) return word;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = idx; i >= 0; i--) {
+            sb.append(word.charAt(i));
+        }
+
+        for (int i = idx + 1; i < word.length(); i++) {
+            sb.append(word.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
